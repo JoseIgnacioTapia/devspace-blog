492804092bba38ffe359aa31133e6ab38564c47f
JoseIgnacioTapia <josignaciot@gmail.com>
2022-01-18 17:46:55 -0300

    Header & Tailwind Styling

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Header from './Header';
 
 function Layout({ title, keywords, description, children }) {
   return (
@@ -9,7 +10,7 @@ function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Header />
       <main className="container mx-auto my-7">{children}</main>
     </div>
   );
